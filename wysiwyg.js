147807704131a9949b274c4f83574b35186ec577
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-06-26 18:30:09 -0500

    making famous people array. adding pics of famous people. getting con error on first obj in 1st arr- : unepexted token... maybe everything needs quotes

---
+++ wysiwyg.js
@@ -0,0 +1,50 @@
+//  var counter = 0;
+// var outputEl = document.getElementById("outputEl");
+// for (; counter < 5; counter++) {
+//   // Give each person element a unique identifier
+//   outputEl.innerHTML += `<div class="person__container" id="person--${counter}"></div>`;
+// }
+
+// // Now containerEl will have elements in it
+// var containerEl = document.getElementsByClassName("person__container");
+
+// // Event listeners are created
+// for (var i = 0; i < containerEl.length; i++) {
+//   containerEl[i].addEventListener("click", function (event) {
+//     // Logic to execute when the element is clicked
+//   });
+// });
+
+// famous person array
+{
+	
+	{
+	  title: "The Man in Black",
+	  name: "Johnny Cash",
+	  bio: "Johnny Cash, the Man in Black, was a singer, guitarist and songwriter whose music innovatively mixed country, rock, blues and gospel influences.",
+	  image: "johnnyCashKitten.jpg",
+	  lifespan: {
+	    birth: 1932,
+	    death: 2003
+	 	}
+	},
+	{
+	  title: "Coco Chanel",
+	  name: "Gabrielle Bonheur",
+	  bio: "Coco Chanel was a leading French modernist designer, whose patterns of simplicity and style revolutionised women’s clothing. She was the only designer to be listed in the Time 100 most influential people of the Twentieth Century.",
+	  image: "cocoChanel.jpg",
+	  lifespan: {
+	    birth: 1883,
+	    death: 1971
+	  }
+	},
+	{
+	  title: "Queen of Country",
+	  name: "Dolly Parton",
+	  bio: "Singer and musician Dolly Parton is a beloved cultural icon whose powerful voice, songwriting and personality have won the hearts of country and pop music fans for decades. She has also appeared in an array of screen projects.",
+	  image: "dolly-parton.jpg",
+	  lifespan: {
+	    birth: 1946,
+	    death: NA
+	  }
+}
